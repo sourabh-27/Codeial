91f452beefa3849c27f17780fac6abe92c30c0d4
Sourabh Majumdar <majumdars844@gmail.com>
2020-06-11 16:39:21 +0530

    Craete manual authentication of the user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,25 @@
 const User = require('../models/user');
 // console.log("I am inside users controller");
 module.exports.profile = function(req, res){
-    return res.render('user_profile',{
-        title: "Users"
-    });
+    console.log("Cookies", req.cookies.user_id);
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+
+            if(err){
+                console.log("Error in finding the details of the user", err);
+                return;
+            }
+
+            if(user){
+                res.render('user_profile', {
+                    title: "User Profile",
+                    user: user
+                });
+            }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 //render the sign up page
@@ -52,6 +68,31 @@ module.exports.create = function(req, res){
 
 //sign in and create a session for the user
 module.exports.createSession = function(req, res){
-    //todo later
+
+    //steps to authenticate
+    //find the user
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log("Error in creating user while signing in"); return;
+        }
+
+        //handle user found 
+        if(user){
+            //handle password which don't match
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }
+        else{
+            //handle user not found
+            return res.redirect('back');
+        }
+    
+    });
 
 }
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const port = 8000;
-
-const app = express();
 
 app.use(express.urlencoded());
 app.use(cookieParser());
